c5e4e055fc191e6cd2db7e25e7e07c1e451af927
Zen <zxc82990@gmail.com>
2016-09-01 05:23:07 +0800

    更新元件來源

--- flappybird.js
+++ flappybird.js
@@ -2,15 +2,16 @@ var game = new Phaser.Game(300 ,500 , Phaser.AUTO, 'phaser', {preload: preload, 
 
 function preload() {
 
-    game.load.spritesheet('bird', 'flappy/bird.png', 34, 24);
-    game.load.spritesheet('pipes', 'flappy/pipes.png', 54, 320);
-    game.load.spritesheet('medals', 'flappy/medals.png', 44, 46);
-    game.load.image('background', 'flappy/background.png');
-    game.load.image('ground', 'flappy/ground.png');
-    game.load.image('logo', 'flappy/title.png');
-    game.load.image('start', 'flappy/start-button.png');
-    game.load.image('over', 'flappy/gameover.png');
-    game.load.image('scoreboard', 'flappy/scoreboard.png');
+    game.load.crossOrigin = "anonymous";
+    game.load.spritesheet('bird', 'http://s.ntustcoding.club/flappybird/bird.png', 34, 24);
+    game.load.spritesheet('pipes', 'http://s.ntustcoding.club/flappybird/pipes.png', 54, 320);
+    game.load.spritesheet('medals', 'http://s.ntustcoding.club/flappybird/medals.png', 44, 46);
+    game.load.image('background', 'http://s.ntustcoding.club/flappybird/background.png');
+    game.load.image('ground', 'http://s.ntustcoding.club/flappybird/ground.png');
+    game.load.image('logo', 'http://s.ntustcoding.club/flappybird/title.png');
+    game.load.image('start', 'http://s.ntustcoding.club/flappybird/start-button.png');
+    game.load.image('over', 'http://s.ntustcoding.club/flappybird/gameover.png');
+    game.load.image('scoreboard', 'http://s.ntustcoding.club/flappybird/scoreboard.png');
 
 };
 
