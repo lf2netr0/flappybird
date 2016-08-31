8e95d091a293abe108c66ed80493ed3cbeafca60
Zen <zxc82990@gmail.com>
2016-08-31 14:49:25 +0800

    準備專案(3)

--- flappybird.js
+++ flappybird.js
@@ -2,15 +2,15 @@ var game = new Phaser.Game(300 ,500 , Phaser.AUTO, 'phaser', {preload: preload, 
 
 function preload() {
 
-    game.load.spritesheet('fish','assets/flappy/bird.png',34,24);
-    game.load.image('background', 'assets/flappy/background.png');
-    game.load.spritesheet('pipes_up', 'assets/flappy/pipes.png',54, 320);
-    game.load.spritesheet('pipes_down', 'assets/flappy/pipes.png',54,320,2);
-    game.load.image('ground', 'assets/flappy/ground.png');
-    game.load.image('logo', 'assets/flappy/title.png');
-    game.load.image('start', 'assets/flappy/start-button.png');
-    game.load.image('over', 'assets/flappy/gameover.png');
-    game.load.image('scoreboard', 'assets/flappy/scoreboard.png');
+    game.load.spritesheet('fish', 'flappy/bird.png', 34, 24);
+    game.load.spritesheet('pipesUp', 'flappy/pipes.png', 54, 320);
+    game.load.spritesheet('pipesDown', 'flappy/pipes.png', 54, 320, 2);
+    game.load.image('background', 'flappy/background.png');
+    game.load.image('ground', 'flappy/ground.png');
+    game.load.image('logo', 'flappy/title.png');
+    game.load.image('start', 'flappy/start-button.png');
+    game.load.image('over', 'flappy/gameover.png');
+    game.load.image('scoreboard', 'flappy/scoreboard.png');
 };
 
 function create(){
