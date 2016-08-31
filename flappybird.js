e47b59fcd354a7b69b5b16d1eb0fd0fdbc7380e2
Zen <zxc82990@gmail.com>
2016-08-31 17:13:03 +0800

    補圖

--- flappybird.js
+++ flappybird.js
@@ -19,89 +19,89 @@ var pipesDown = new Array();
 var score = 0;
 
 function create(){
-	background = game.add.tileSprite(0, 0, 300, 500, 'background');
+    background = game.add.tileSprite(0, 0, 300, 500, 'background');
 
-	ground = game.add.tileSprite(0, 400, 335, 112, 'ground');
-	ground.autoScroll(-200, 0);
-	game.physics.enable(ground, Phaser.Physics.ARCADE);
+    ground = game.add.tileSprite(0, 400, 335, 112, 'ground');
+    ground.autoScroll(-200, 0);
+    game.physics.enable(ground, Phaser.Physics.ARCADE);
 
-	bird = game.add.sprite(125, 250, 'bird');
-	game.physics.enable(bird, Phaser.Physics.ARCADE);
-	bird.body.gravity.y = 1200;
+    bird = game.add.sprite(125, 250, 'bird');
+    game.physics.enable(bird, Phaser.Physics.ARCADE);
+    bird.body.gravity.y = 1200;
 
-	generatePipes();
+    generatePipes();
 
-	cursors = game.input.keyboard.createCursorKeys();
-	jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-	logo = game.add.sprite(60, 100, 'logo');
+    logo = game.add.sprite(60, 100, 'logo');
     button = game.add.button(100, 250, 'start', startGame);
-	gamePause();
+    gamePause();
 
 };
 
 function update(){
 
-	game.physics.arcade.overlap(bird,  pipesUp, dead, null, this);
-	game.physics.arcade.overlap(bird,  pipesDown, dead, null, this);
-	game.physics.arcade.overlap(bird,  ground, dead, null, this);
+    game.physics.arcade.overlap(bird,  pipesUp, dead, null, this);
+    game.physics.arcade.overlap(bird,  pipesDown, dead, null, this);
+    game.physics.arcade.overlap(bird,  ground, dead, null, this);
 
-	if(bird.alive){
-		if (jumpButton.isDown || game.input.activePointer.leftButton.isDown)
-	    {
-	        bird.body.velocity.y = -350;
-	    }
+    if(bird.alive){
+        if (jumpButton.isDown || game.input.activePointer.leftButton.isDown)
+        {
+            bird.body.velocity.y = -350;
+        }
 
-		for(var i = 0;i < 2;i++){
-			if(pipesUp[i].body.x < -100){
-				var position = 180 * Math.random();
-				pipesUp[i].body.x = 300;
-				pipesUp[i].body.y = -270 + position;
-				pipesDown[i].body.x = 300;
-				pipesDown[i].body.y = 190 + position;
-				score += 1;
-			}
-		}
-	}
+        for(var i = 0;i < 2;i++){
+            if(pipesUp[i].body.x < -100){
+                var position = 180 * Math.random();
+                pipesUp[i].body.x = 300;
+                pipesUp[i].body.y = -270 + position;
+                pipesDown[i].body.x = 300;
+                pipesDown[i].body.y = 190 + position;
+                score += 1;
+            }
+        }
+    }
 
 };
 
 function generatePipes(){
 
-	for(var i = 0;i < 2;i++){
+    for(var i = 0;i < 2;i++){
 
-		var position = 180 * Math.random();
-		var pipesup = game.add.sprite(300 + i*200, -270 + position, 'pipes');
-		var pipesdown = game.add.sprite(300 + i*200, 190 + position, 'pipes', 1);
-		game.physics.enable(pipesup, Phaser.Physics.ARCADE);
-		game.physics.enable(pipesdown, Phaser.Physics.ARCADE);
-		pipesup.body.velocity.x = -100;
-		pipesdown.body.velocity.x = -100;
+        var position = 180 * Math.random();
+        var pipesup = game.add.sprite(300 + i*200, -270 + position, 'pipes');
+        var pipesdown = game.add.sprite(300 + i*200, 190 + position, 'pipes', 1);
+        game.physics.enable(pipesup, Phaser.Physics.ARCADE);
+        game.physics.enable(pipesdown, Phaser.Physics.ARCADE);
+        pipesup.body.velocity.x = -100;
+        pipesdown.body.velocity.x = -100;
 
-		pipesUp.push(pipesup);
-		pipesDown.push(pipesdown);
+        pipesUp.push(pipesup);
+        pipesDown.push(pipesdown);
 
-	}
-	
+    }
+    
 
 }
 
 function dead(){
 
-	bird.destroy();
-	over = game.add.sprite(55, 100, 'over');
-	over.bringToTop();
-	scoreboard = game.add.sprite(35, 150, 'scoreboard');
-	var style = { font: 'bold 50pt Arial', fill: 'white', align: 'left'};
+    bird.destroy();
+    over = game.add.sprite(55, 100, 'over');
+    over.bringToTop();
+    scoreboard = game.add.sprite(35, 150, 'scoreboard');
+    var style = { font: 'bold 50pt Arial', fill: 'white', align: 'left'};
     var text = game.add.text(175, 190, score, style);
     if (score >= 10 && score < 20){
-    	medals = game.add.sprite(63, 193, 'medals');
+        medals = game.add.sprite(63, 193, 'medals');
     }
     else if(score >=20){
-    	medals = game.add.sprite(63, 193, 'medals', 1);
+        medals = game.add.sprite(63, 193, 'medals', 1);
     }
 
-}
+}   
 
 function gamePause() {
 
@@ -111,8 +111,8 @@ function gamePause() {
 
 function startGame(){
 
-	logo.kill();
-	button.kill();
-	gamePause();
+    logo.kill();
+    button.kill();
+    gamePause();
 
 }
